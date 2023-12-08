8a3f74ee7e39a2fdd41475aa586c5267acc4ce60
mohamed_sameh <ms627673@gmail.com>
2023-12-08 11:12:16 +0200

    join us page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+
   const fileDropArea = document.getElementById("fileDropArea");
   const fileInput = document.getElementById("fileInput");
   const fileNameElement = document.getElementById("fileName");
